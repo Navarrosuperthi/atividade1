bb2f02c9a28144f78e2a4b1e2fd92d687b29a3fa
FabioBritto <fabio.tritono@gmail.com>
2024-06-04 16:11:36 -0300

    Commit project atividade1

---
+++ src/ProblemSet2_1.java
@@ -0,0 +1,22 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        System.out.println(" X              X");
+        System.out.println("X  XXX       XXX  X");
+        System.out.println(" X  x         x  X ");
+        System.out.println("  X     XXX     X   ");
+        System.out.println("   X  XX   XX  X   ");
+        System.out.println("    X  XX XX  X  ");
+        System.out.println("     X  XXXX X     ");
+    }    
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+       
+
